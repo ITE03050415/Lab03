f0da3396e24bded19e015affb439224c8c5f486e
Joe <joe@joe-VirtualBox.(none)>
2016-09-19 17:08:10 +0800

    9/19

---
+++ lab03.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;
+printf("Please enter 10 numbers");
+scanf("%d %d %d %d %d %d %d %d %d %d",&n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9,&n10);
+printf("Your numbers : %d %d %d %d %d %d %d %d %d %d",n1,n2,n3,n4,n5,n6,n7,n8,n9,n10);
+}
